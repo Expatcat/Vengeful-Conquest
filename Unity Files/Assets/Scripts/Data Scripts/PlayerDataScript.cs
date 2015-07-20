cf107e644ab41e275e492b0d6a3a9255a9ad0c8c
Expatcat <c3smith@ucsd.edu>
2015-07-19 23:11:44 -0700

    Added camera follow to player
    - Camera now follows player
    - Also added some player scripts / stats fields

---
+++ Unity Files/Assets/Scripts/Data Scripts/PlayerDataScript.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerDataScript : MonoBehaviour {
+
+	public GameObject player;
+	
+	void Start() {
+	
+	}
+
+	// Use this for initialization
+	void OnLevelWasLoaded () {
+	
+	  if (Application.loadedLevelName == "MainMenu") {
+	    hidePlayer ();
+	  }
+	  
+	  else {
+	  
+	    showPlayer ();
+	  }
+	
+	  
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	  Camera.main.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, Camera.main.transform.position.z );
+	
+	}
+	
+	void hidePlayer() {
+	
+      player.GetComponent<Renderer>().enabled = false;
+	
+	}
+	
+	void showPlayer() {
+	
+	  player.GetComponent<Renderer>().enabled = true;
+	
+	}
+}
---
+++ Unity Files/Assets/Scripts/PlayerData.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerData : MonoBehaviour {
+
+	public int health;
+	public float experience;
+	public int level;
+
+	public float playerWorldLocation, playerBattleLocation;
+        
+	// Use this for initialization
+	void Start () {
+	
+	GetComponent<Renderer>().enabled = false;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
