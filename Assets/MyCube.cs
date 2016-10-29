9b13547ca717ad791ef93cacd42a9fd198222eb3
connor-a-smith <c3smith@ucsd.edu>
2016-10-29 10:44:16 -0700

    Latest push

---
+++ Assets/MyCube.cs
@@ -0,0 +1,117 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using System;
+
+public class MyCube : MonoBehaviour
+{
+	private string saveDirectory;
+	private string saveName;
+
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+		saveDirectory = Application.persistentDataPath + "/Saved Data/MyCube/";
+		saveName = saveDirectory + "cube.dat";
+
+		Load ();
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+		Save ();
+
+	}
+
+	void Save ()
+	{
+		BinaryFormatter formatter = new BinaryFormatter ();
+
+		if (!Directory.Exists (saveDirectory)) {
+
+			Directory.CreateDirectory (saveDirectory);
+
+		}
+
+		FileStream file = File.Create (saveName);
+
+		PersistentCube cubeToSave = new PersistentCube ();
+
+		Color cubeColor = this.GetComponent<MeshRenderer> ().material.color;
+		cubeToSave.SetCubeColor (new Vector3 (cubeColor.r, cubeColor.g, cubeColor.b));
+
+		Vector3 cubePosition = this.transform.position;
+		cubeToSave.SetCubePosition (cubePosition);
+
+		formatter.Serialize (file, cubeToSave);
+		file.Close ();
+	}
+
+	void Load ()
+	{
+
+		if (File.Exists (saveName)) {
+
+			BinaryFormatter formatter = new BinaryFormatter ();
+			FileStream file = File.Open (saveName, FileMode.Open);
+			PersistentCube loadedCube = (PersistentCube)formatter.Deserialize (file);
+			file.Close ();
+
+			this.transform.position = loadedCube.GetCubePosition ();
+
+			Vector3 loadedColorVals = loadedCube.GetCubeColor ();
+			Color loadedColor = new Color (loadedColorVals.x, loadedColorVals.y, loadedColorVals.z);
+			this.GetComponent<MeshRenderer> ().material.SetColor ("_Color", loadedColor);
+
+
+		}
+	}
+}
+
+
+[Serializable]
+class PersistentCube
+{
+
+	float[] cubePosition = new float[3];
+	float[] cubeColor = new float[3];
+
+	public void SetCubeColor (Vector3 color)
+	{
+
+		cubeColor [0] = color.x;
+		cubeColor [1] = color.y;
+		cubeColor [2] = color.z;
+
+	}
+
+	public void SetCubePosition (Vector3 position)
+	{
+		cubePosition [0] = position.x;
+		cubePosition [1] = position.y;
+		cubePosition [2] = position.z;
+
+	}
+
+	public Vector3 GetCubePosition ()
+	{
+
+		return new Vector3 (cubePosition [0], cubePosition [1], cubePosition [2]);
+
+	}
+
+	public Vector3 GetCubeColor ()
+	{
+
+		return new Vector3 (cubeColor [0], cubeColor [1], cubeColor [2]);
+
+	}
+
+
+}
--- Assets/Scripts/Data Scripts/DataScript.cs
+++ Assets/Scripts/Data Scripts/DataScript.cs
@@ -4,158 +4,161 @@ using System;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.IO;
 
-public class DataScript : MonoBehaviour {
+public class DataScript : MonoBehaviour
+{
 
-  public static DataScript data;
+	public static DataScript data;
   
 	public ArmyDataScript armyData;
 	public KingdomDataScript kingdomData;
 	public PlayerDataScript playerData;
 
-  public int mainMenuSceneNumber;
-  public int openingSceneNumber;
-  public int worldSceneNumber;
-  public int battlefieldSceneNumber;
+	public int mainMenuSceneNumber;
+	public int openingSceneNumber;
+	public int worldSceneNumber;
+	public int battlefieldSceneNumber;
   
-  private int currentScene;
+	private int currentScene;
   
-  /* Save Locations */
-  [HideInInspector]
-  public static string dataDir = Application.persistentDataPath + "/Saved Data/";
-  public static string gameDir = dataDir + "Game Data/";
-  public static string armyDir = dataDir + "Army Data/";
-  public static string kingdomDir = dataDir + "Kingdom Data/";
-  public static string soldierDir = armyDir + "Soldier Data/";
-  public static string castleDir = kingdomDir + "Castle Data/";
+	/* Save Locations */
+	[HideInInspector]
+	public static string dataDir = Application.persistentDataPath + "/Saved Data/";
+	public static string gameDir = dataDir + "Game Data/";
+	public static string armyDir = dataDir + "Army Data/";
+	public static string kingdomDir = dataDir + "Kingdom Data/";
+	public static string soldierDir = armyDir + "Soldier Data/";
+	public static string castleDir = kingdomDir + "Castle Data/";
   
-  public static string gameFile = gameDir + "gameData.dat";
-  public static string armyFile = armyDir + "armyData.dat";
-  public static string soldierFile = soldierDir + "soldierData";
-  public static string kingdomFile = kingdomDir + "kingdomData.dat";
-  public static string castleFile = castleDir + "castleData";
+	public static string gameFile = gameDir + "gameData.dat";
+	public static string armyFile = armyDir + "armyData.dat";
+	public static string soldierFile = soldierDir + "soldierData";
+	public static string kingdomFile = kingdomDir + "kingdomData.dat";
+	public static string castleFile = castleDir + "castleData";
   
-  public string armyManagerKey;
-  
+	public string armyManagerKey;
 
-   void Awake() {
+
+	void Awake ()
+	{
       
-      if (data == null) {
+		if (data == null) {
       
-        DontDestroyOnLoad(this);
-        data = this;
+			DontDestroyOnLoad (this);
+			data = this;
         
-      }
+		} else if (data != this) {
       
-      else if (data != this) {
+			Destroy (gameObject);
       
-        Destroy (gameObject);
-      
-      }
-    }
+		}
+	}
     
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
   
 	
 	}
+
+	void OnLevelWasLoaded ()
+	{
   
-  void OnLevelWasLoaded() {
-  
-    currentScene = Application.loadedLevel;
+		currentScene = Application.loadedLevel;
     
-    if (currentScene == mainMenuSceneNumber) {
+		if (currentScene == mainMenuSceneNumber) {
     
-      KingdomDataScript.kingdomData.SetCastleState(false);
+			KingdomDataScript.kingdomData.SetCastleState (false);
       
-    }
+		}
     
-    if (currentScene == openingSceneNumber) {
+		if (currentScene == openingSceneNumber) {
     
-      KingdomDataScript.kingdomData.SetCastleState(false);
+			KingdomDataScript.kingdomData.SetCastleState (false);
      
-    }
+		} else if (currentScene == worldSceneNumber) {
     
-    else if (currentScene == worldSceneNumber) {
+			KingdomDataScript.kingdomData.SetCastleState (true);
     
-      KingdomDataScript.kingdomData.SetCastleState(true);
+		} else if (currentScene == battlefieldSceneNumber) {
     
-    }
-    
-    else if (currentScene == battlefieldSceneNumber) {
-    
-      KingdomDataScript.kingdomData.SetCastleState(false);
+			KingdomDataScript.kingdomData.SetCastleState (false);
       
-    }
+		}
     
-  }
+	}
   
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 
 
 	}
+
   
   
+	public void SaveData ()
+	{
   
-  public void SaveData() {
+		Save ();
+		armyData.Save ();
+		//castleData.Save();
+		//playerData.Save();
   
-    Save ();
-    armyData.Save ();
-    //castleData.Save();
-    //playerData.Save();
+	}
+
+	public void LoadData ()
+	{
   
-  }
+		Load ();
+		armyData.Load ();
+		//castleData.Load();
+		// playerData.Load();
+		Application.LoadLevel (currentScene);
   
-  public void LoadData() {
-  
-    Load ();
-    armyData.Load ();
-    //castleData.Load();
-   // playerData.Load();
-    Application.LoadLevel (currentScene);
-  
-  }
-  
-  public void Save() {
+	}
+
+	public void Save ()
+	{
     
-    BinaryFormatter formatter = new BinaryFormatter();
+		BinaryFormatter formatter = new BinaryFormatter ();
+ 
+		Directory.CreateDirectory (dataDir);
+		Directory.CreateDirectory (gameDir);
     
-    Directory.CreateDirectory(dataDir);
-    Directory.CreateDirectory (gameDir);
+		FileStream file = File.Create (gameFile);
     
-    FileStream file = File.Create (gameFile);
+		GameData savedData = new GameData ();
+		savedData.currentScene = currentScene;
     
-    GameData savedData = new GameData();
-    savedData.currentScene = currentScene;
+		/* Data to save goes here */
     
-    /* Data to save goes here */
+		formatter.Serialize (file, savedData);
+		file.Close ();
     
-    formatter.Serialize (file, savedData);
-    file.Close();
+	}
+
+	public void Load ()
+	{
     
-  }
-  
-  public void Load() {
-    
-    if (File.Exists(gameFile))
-    {
+		if (File.Exists (gameFile)) {
       
-      BinaryFormatter formatter = new BinaryFormatter();
-      FileStream file = File.Open (gameFile, FileMode.Open);
-      GameData savedData = (GameData)formatter.Deserialize (file);
-      file.Close ();
+			BinaryFormatter formatter = new BinaryFormatter ();
+			FileStream file = File.Open (gameFile, FileMode.Open);
+			GameData savedData = (GameData)formatter.Deserialize (file);
+			file.Close ();
       
-      currentScene = savedData.currentScene;
+			currentScene = savedData.currentScene;
    
-    }
-  }
+		}
+	}
 }
 
 [Serializable]
-class GameData {
+class GameData
+{
 
-  public int currentScene;
+	public int currentScene;
 
 
 }
